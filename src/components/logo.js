b340f4899bc9b38c001c88cadf057e9166f096f5
foosayyy <dankrish26@gmail.com>
2022-04-11 20:29:15 +0530

    removed logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,17 +1,17 @@
-import react from "react"
-import img1 from "../favicon_io/android-chrome-512x512.png"
+// import react from "react"
+// import img1 from "../favicon_io/android-chrome-512x512.png"
 
-const customStyle = {
-    width: '100px' ,
-    height: '100px' ,
-};
+// const customStyle = {
+//     width: '100px' ,
+//     height: '100px' ,
+// };
 
-function Logo() {
-    return(
-        <div className="logo" style={customStyle}>
-            <img src={img1} alt="logo"></img>
-        </div>
-    );
-}
+// function Logo() {
+//     return(
+//         <div className="logo" style={customStyle}>
+//             <img src={img1} alt="logo"></img>
+//         </div>
+//     );
+// }
 
-export default Logo;
+// export default Logo;
